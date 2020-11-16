21130c446b49d3ab39580bd093bf4cb7e014fc72
jcspovieri <jcspovieri@gmail.com>
2020-11-16 14:37:11 -0300

    Criado banco de dados de imagens e seus acessos

--- app/src/main/java/com/example/bread/ui/feddActivity2.kt
+++ app/src/main/java/com/example/bread/ui/feddActivity2.kt
@@ -1,16 +1,19 @@
 package com.example.bread.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bread.R
-import kotlinx.android.synthetic.main.activity_fedd2.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
+private var mStorageRef: StorageReference? = null
 
 class feddActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fedd2)
-
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
 
 
