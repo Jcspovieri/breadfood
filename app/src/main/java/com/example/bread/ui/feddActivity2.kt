998951e612a9a20261beaaf10eb5502d08f0822e
jcspovieri <jcspovieri@gmail.com>
2020-11-18 17:56:34 -0300

    Icone criado e adicionado ao prejeto

--- app/src/main/java/com/example/bread/ui/feddActivity2.kt
+++ app/src/main/java/com/example/bread/ui/feddActivity2.kt
@@ -1,15 +1,21 @@
 package com.example.bread.ui
 
+
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bread.R
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import java.io.File
+
 
 
 private var mStorageRef: StorageReference? = null
 
 class feddActivity2 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fedd2)
@@ -17,5 +23,10 @@ class feddActivity2 : AppCompatActivity() {
 
 
 
+
+
+
         }
+
     }
+
